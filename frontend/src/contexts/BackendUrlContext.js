a989e31ffbda7507f3e1ec9dc31bea426199fbdb
Aditya Rao C <adityaraochokkadi@gmail.com>
2026-01-19 16:51:40 +0530

    Frontend: Change backend url provided by BackendUrlContext
    After deploying the spring boot app in render via the deploy web
    service feature, it gave the url to be used to access the web service.
    The backend url provided by BackendUrlContext was changed to this url.

--- frontend/src/contexts/BackendUrlContext.js
+++ frontend/src/contexts/BackendUrlContext.js
@@ -3,7 +3,8 @@ import * as React from "react";
 export let BackendUrlContext=React.createContext();
 
 export function BackendUrlProvider({children}) {
-  let backendUrl="http://localhost:8080";
+  let backendUrl="https://notes-app-backend-fxov.onrender.com";
+  // let backendUrl="http://localhost:8080";
   // let backendUrl="http://192.168.1.104:8080";
 
   return (
